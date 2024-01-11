73e2d584cec86d01344178e1982b97da0b5a18ec
Aryan <aryandoshi03@gmail.com>
2024-01-11 10:45:47 +0530

    Theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mac_store/utils/theme/custom_theme/appBar_theme.dart';
+import 'package:mac_store/utils/theme/custom_theme/bottomSheet_theme.dart';
+import 'package:mac_store/utils/theme/custom_theme/checkbox_theme.dart';
+import 'package:mac_store/utils/theme/custom_theme/chip_theme.dart';
+import 'package:mac_store/utils/theme/custom_theme/elevatedButton_theme.dart';
+import 'package:mac_store/utils/theme/custom_theme/outlinedButton_theme.dart';
 import 'package:mac_store/utils/theme/custom_theme/text_theme.dart';
+import 'package:mac_store/utils/theme/custom_theme/textfield_theme.dart';
 
 class MAppTheme {
   MAppTheme._();
@@ -9,15 +16,30 @@ class MAppTheme {
     fontFamily: 'Poppins',
     brightness: Brightness.light,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
     textTheme: MTextTheme.lightTextTheme,
+    chipTheme: MChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: MAppBarTheme.lightAppBarTheme,
+    checkboxTheme: MCheckBoxTheme.lightCheckBoxTheme,
+    bottomSheetTheme: MBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: MElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: MOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: MTextFieldTheme.lightInputDecorationTheme,
   );
+
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.black,
     textTheme: MTextTheme.darkTextTheme,
+    chipTheme: MChipTheme.darkChipTheme,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: MAppBarTheme.darkAppBarTheme,
+    checkboxTheme: MCheckBoxTheme.darkCheckBoxTheme,
+    bottomSheetTheme: MBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: MElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: MOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: MTextFieldTheme.darkInputDecorationTheme,
   );
 }
